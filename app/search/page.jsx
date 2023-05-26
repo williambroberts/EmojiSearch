28d8335fbd03aba1ce2eee3dece5bc7d75553aef
= <=3williamroberts@gmail.com>
2023-05-26 12:34:55 +0100

    r 6

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -14,6 +14,7 @@ const SearchPage = () => {
   const [pagnationLength,setPagnationLength]=useState(10)
   const [results,setResults]=useState(undefined)
   const disallowedSearched = ["",undefined," "]
+  const [isDisabled,setIsDisabled]=useState(false)
   const handleSearch = (e)=> {
     console.log(e.target.value)
     if (e.target.value<3){
@@ -27,25 +28,50 @@ const SearchPage = () => {
     SetSearchText(e.target.value.toLowerCase())
     let newResults = emojis.filter((item,index)=> item.name.toLowerCase().includes(searchText))
     setResults(newResults)
+    setEndIndex((prev)=>pagnationLength)
+    setStartIndex(0)
   }
 
-  const handlePagnation = (e)=>{
-    
-    if (e.target.value>50){
-      setPagnationLength(50)
-      setEndIndex(50)
-      setStartIndex(0)
-    }else if (e.target.value<0){
-      setPagnationLength(1)
-      setEndIndex(1)
-      setStartIndex(0)
-    }else {
-      setPagnationLength(parseInt(e.target.value))
-      setEndIndex(parseInt(e.target.value))
-      setStartIndex(0)
+  const handlePagnation = (num)=>{
+    console.log(num,typeof(num),EndIndex,startIndex)
+    if (num===NaN){
+      
+      return
     }
+    if (num>50){
+      setPagnationLength(50)
+      if (results!==undefined){
+         if ((EndIndex+num-pagnationLength)>results.length){
+            setEndIndex(results.length)
+         }else {
+         setEndIndex((prev)=>prev+num-pagnationLength)
+         }
+        
+      }
+     
+   
+    }else if (num<0){
+      setPagnationLength(1)
+      setEndIndex((prev)=>startIndex+1)
+     
+    }else {
+      
+      if (results!==undefined){
+        if ((EndIndex+num-pagnationLength)>results.length){
+           setEndIndex(results.length)
+        }else {
+        setEndIndex((prev)=>prev+num-pagnationLength)
+        }
+      
+      
+    }else{
+      setEndIndex(pagnationLength)
+      setStartIndex(0)
 
+    }
+  setPagnationLength(num)
   }
+}
   const handlePrev = () =>{
     if (startIndex <pagnationLength){
       setStartIndex(0)
@@ -56,6 +82,9 @@ const SearchPage = () => {
     }
   }
   const handleNext = ()=>{
+    if(results===undefined){
+      return
+    }
     if ((EndIndex+pagnationLength)>results.length){
       setEndIndex(results.length)
       setStartIndex((prev)=>results.length-pagnationLength)
@@ -73,13 +102,13 @@ const SearchPage = () => {
       <input className='search-input'
       type='text' placeholder='Search for an Emoji' value={searchText} onChange={(e)=>handleSearch(e)}/>
       <FlexRow>
-        <button className='' onClick={()=>handlePrev()}>prev</button>
-      <input type='number' min="1" max="50" name='results-length' value={pagnationLength} onInput={(e)=>handlePagnation(e)}/>
-      <button className='' onClick={()=>handleNext()}>Next</button>
+        <button className='' onClick={()=>handlePrev()} disabled={isDisabled}>prev</button>
+      <input type='number' min="1" max="50" name='results-length' value={pagnationLength} onInput={(e)=>handlePagnation(parseInt(e.target.value))}/>
+      <button className='' onClick={()=>handleNext()} disabled={isDisabled}>Next</button>
       </FlexRow>
       <FlexRow>
-           <span>{disallowedSearched.includes(searchText)? "":searchText.length<3? "": `${results.length } Emojis found`} </span>
-           {results===undefined? "": <span>showing results {startIndex+1}:{EndIndex}</span>}
+           <span>{disallowedSearched.includes(searchText)? "_":searchText.length<3? "_": `${results.length } Emojis found`} </span>
+           {results===undefined? "":searchText.length<3? "_": <span>showing results {startIndex+1}:{EndIndex}</span>}
       </FlexRow>
    
 
